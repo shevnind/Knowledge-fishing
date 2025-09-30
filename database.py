0370d9d77ff5ee0fbf7bd227a59181c99619e097
Даниил Шевнин <shevnin.dm@phystech.edu>
2025-09-30 22:09:14 +0300

    try to save database

--- database.py
+++ database.py
@@ -1,8 +1,11 @@
+import os
+
 from sqlmodel import create_engine, SQLModel
 from typing import TYPE_CHECKING
 
 
-sqlite_url = 'sqlite:///database.db'
+db_path = os.getenv('DATABASE_PATH', '/data/database.db')
+sqlite_url = f'sqlite:///{db_path}'
 engine = create_engine(sqlite_url, echo=True)
 
 def create_table():
