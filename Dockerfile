283dca6ce794ee2a7cf19388b0ec93523da32564
Даниил Шевнин <shevnin.dm@phystech.edu>
2025-10-24 15:56:26 +0300

    change using ssl

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,6 @@ RUN git clone --depth 1 https://github.com/koldun256/knowledge-fishing /frontend
 WORKDIR /frontend
 RUN npm install && npm run build
 
-
-
 FROM python:3.12
 
 RUN apt update && apt install -y git
@@ -13,10 +11,16 @@ RUN apt update && apt install -y git
 RUN git clone --depth 1 https://github.com/shevnind/Knowledge-fishing /backend
 
 WORKDIR /backend
+
 RUN pip install -r requirements.txt
 
 RUN mkdir build
 COPY --from=frontend /frontend/build ./build
 
+COPY certificate.crt /backend/ssl/
+COPY certificate_ca.crt /backend/ssl/
+COPY certificate.key /backend/ssl/
+
 EXPOSE 8000
+EXPOSE 443
 CMD ["python3", "main.py"]
--- main.py
+++ main.py
@@ -4,36 +4,16 @@ import ssl
 from http.server import HTTPServer
 
 
-# if __name__ == "__main__":
-#     create_table()
-
-#     import uvicorn
-
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
-
-
 if __name__ == "__main__":
-    # Проверяем аргументы командной строки
-    import sys
-    ssl_enabled = "--ssl-certfile" in sys.argv
-    
-    if ssl_enabled:
-        certfile_index = sys.argv.index("--ssl-certfile") + 1
-        keyfile_index = sys.argv.index("--ssl-keyfile") + 1
-        # certfile = sys.argv[certfile_index]
-        # keyfile = sys.argv[keyfile_index]
-        certfile = os.path.join("ssl/combined.pem")
-        keyfile = os.path.join("ssl/private.key")
-        
-        # Создаем SSL контекст
-        context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-        context.load_cert_chain(certfile, keyfile)
-        
-        server = HTTPServer(('0.0.0.0', 443), app)  # HTTPS на порту 443
-        server.socket = context.wrap_socket(server.socket, server_side=True)
-        print("Starting HTTPS server on port 443...")
-    else:
-        server = HTTPServer(('0.0.0.0', 8000), app)
-        print("Starting HTTP server on port 8000...")
-    
-    server.serve_forever()
+    create_table()
+
+    import uvicorn
+
+    uvicorn.run(
+        app, 
+        host="0.0.0.0",
+        port=443,  # HTTPS порт
+        ssl_keyfile="ssl/certificate.key",
+        ssl_certfile="ssl/certificate.crt",
+        ssl_ca_certs="ssl/certificate_ca.crt"  # опционально
+    )
